3bb34d383baf99c225ad0fe6febabd9046efdfee
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2025-02-08 13:55:56 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,12 @@
+const isDev = process.argv.indexOf("dev") !== -1;
+const isBuild = process.argv.indexOf("build") !== -1;
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = "1";
+  const { build } = await import("velite");
+  await build({ watch: isDev, clean: !isDev });
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: { 
-      rulesWithoutResolveExtensions: true
-    }, 
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin())
-    return config
-  }
-}
-
-class VeliteWebpackPlugin {
-  static started = false
-  apply(/** @type {import('webpack').Compiler} */ compiler) { 
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      const { build } = await import('velite')
-      await build({ watch: dev, clean: !dev })
-    })
-  }
-}
-
-export default nextConfig
+export default {
+  typescript: { ignoreBuildErrors: true },
+};
